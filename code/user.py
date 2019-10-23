3b5f7c8b1a1501ec6b341a0dd94843d8dce74706
Joseorina <orinangare@gmail.com>
2019-10-23 03:36:05 +0300

    find by username and id

--- code/user.py
+++ code/user.py
@@ -1,5 +1,14 @@
+import sqlite3
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+        
+    @classmethod
+    def find_by_username(cls, username):
+        pass
+    
+    @classmethod
+    def find_by_id(cls, _id):
+        pass        
