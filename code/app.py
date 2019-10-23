8695a19bb87e442dcf513fbf1418938d568d3b4d
Joseorina <orinangare@gmail.com>
2019-10-23 03:21:03 +0300

    imports

--- code/app.py
+++ code/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource,Api, reqparse
 from flask_jwt import  JWT, jwt_required
 
-from security import authenticate, identity
+from .security import authenticate, identity
 
 app = Flask(__name__)
 api = Api(app)
