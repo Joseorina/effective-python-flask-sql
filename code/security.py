d196380b1c938bd5f3d014308b29c4d0720ccc4c
Joseorina <orinangare@gmail.com>
2019-10-23 03:21:21 +0300

    security

--- code/security.py
+++ code/security.py
@@ -1,5 +1,5 @@
 from werkzeug.security import safe_str_cmp
-from user import User
+from .user import User
 
 users = [
     User(1, 'bob','asdf ')
@@ -10,10 +10,28 @@ username_mapping = {u.username: u for u in users}
 userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
+    """
+    Generate JWT token
+    
+    Arguments:
+        username {[type]} -- [description]
+        password {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
     user = userid_mapping.get(username, None)
     if user and safe_str_cmp(user.password, password):
         return user
     
 def identity(payload):
+    """
+    Authenticate an endpoint    
+    Arguments:
+        payload {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
     user_id = payload['identity']
     return userid_mapping(user_id, None)
