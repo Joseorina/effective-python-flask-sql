096df9c5bfe16f9feb61808ae138d43e548db2b9
Joseorina <orinangare@gmail.com>
2019-10-23 03:14:40 +0300

    insert, delete and retrieve items

--- test.py
+++ test.py
@@ -13,6 +13,17 @@ user = (1, 'jose', 'asdf')
 insert_query = "INSERT INTO users VALUES (?,?,?)"
 cursor.execute(insert_query, user)
 
+users = [
+    (2, 'rolf', 'asdf'),
+    (3, 'anna', 'xyz')
+]
+#insert many users in data
+cursor.executemany(insert_query, users)
+
+select_query = "SELECT * FROM users"
+for row in cursor.execute(select_query):
+    print(row)
+
 connection.commit()
 
 connection.close()
